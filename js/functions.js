2e61d4430a21193ad4c06569e637dedf844bfaa2
Kate Velikodnaya <emvelikodnaya@gmail.com>
2023-02-21 15:19:28 +0400

    Добавляет функции по 2 дз

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,15 @@
 // Функция, вычисляющая, соответствует ли длина строки заданной
 // возвращает true, если длина строки не превыщает заданную, иначе - false.
-function isStringFits (str, lgth) {
-  if (str.length <= lgth) {
-    return true;
-  }
-  return false;
-}
+const isStringFits = (str, lgth) => str.lenght <= lgth;
 isStringFits ('super ', 5);
+
 
 // Функция, выясняющая, является ли строка полиндромом
 // возвращает true, если строка полиндром, иначе - false.
-function isPolindrom (str) {
-  str = str.toLowerCase();
-  str = str.split(' ').join('');
+function isPalindrom (str) {
+  str = str
+    .toLowerCase()
+    .replaceAll(' ', '');
   let i = 0;
   let j = str.length - 1;
   while (i < j) {
@@ -24,36 +21,39 @@ function isPolindrom (str) {
   }
   return true;
 }
-isPolindrom ('topoT');
+isPalindrom ('topoT');
 
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
 // Возвращает извлеченные цифры в виде целого положительного числа
-const zero = '0'.charCodeAt(0);
 const nine = '9'.charCodeAt(0);
+const zero = '0'.charCodeAt(0);
 
 function getNumbers (str) {
+  if (typeof str === 'number') {
+    return str;
+  }
   let result = '';
-  str = String(str);
   for (let i = 0; i <= str.length - 1; i++) {
     if (str.charCodeAt(i) >= zero && str.charCodeAt(i) <= nine) {
       result += str[i];
     }
   }
   if (result === '') {
-    result = NaN;
+    return NaN;
   }
-  return Number(result);
+  return result;
 }
-getNumbers ('1 кефир, 0.5 батона');
+getNumbers ('1 бутылка кефира 0.5 батона');
+
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами
 // Возвращает исходную строку, дополненную указанными символами до заданной длины.
-function newString (str, qty, extraChars) {
-  if (str.length >= qty) {
+function newString (str, minLength, pad) {
+  const actualPad = minLength - str.length;
+  if (str.length >= minLength) {
     return str;
-  } else {
-    return extraChars.slice(- (extraChars.lenght - 1), qty - str.length) + str;
   }
+  return pad.slice(0, actualPad % pad.length) + pad.repeat(actualPad / pad.length) + str;
 }
-newString ('q', 4, 'werty');
+newString ('qoi', 6, 'we');
