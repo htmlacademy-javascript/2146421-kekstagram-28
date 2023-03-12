e91b0bfe9d8b0d7078ac15700c15682fd42a8af3
Keks <keks@htmlacademy.ru>
2023-03-12 22:53:32 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import { createObjectsArray } from './data.js';
+import { createPictures } from './create-miniatures.js';
+const picturesArray = createObjectsArray();
+createPictures(picturesArray);
