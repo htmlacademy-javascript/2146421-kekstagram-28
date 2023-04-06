098e5c00df08ca63f5c0babf93d34164177eb519
Kate Velikodnaya <emvelikodnaya@gmail.com>
2023-04-06 23:38:07 +0400

    правки

--- js/main.js
+++ js/main.js
@@ -5,18 +5,26 @@ import './image-filters.js';
 import { getData } from './api.js';
 import { loadingErrorMessage } from './util.js';
 import { createPictures } from './create-miniatures.js';
-//import { renderBigPicture } from './big-picture-modal.js';
 import { setUserFormSubmit, closePictureForm } from './form.js';
+import { renderBigPicture } from './big-picture-modal.js';
 
 getData()
   .then((pictures) => {
     createPictures(pictures);
+    renderBigPicture(pictures);
   })
-  .catch(
-    (err) => {
-      loadingErrorMessage(err.message);
-    }
+  .catch((err) => {
+    loadingErrorMessage(err.message);
+  }
   );
+
+// try {
+//   const data = await getData();
+//   createPictures(data);
+//   renderBigPicture(data);
+// } catch (err) {
+//   loadingErrorMessage(err.message);
+// }
 
 setUserFormSubmit(closePictureForm);
 
