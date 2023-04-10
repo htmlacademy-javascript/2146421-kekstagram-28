e1f846bdb2a304aa8e5513e269f77124ca0266e2
Keks <keks@htmlacademy.ru>
2023-04-10 11:51:05 +0300

    :heavy_check_mark: Сборка #13

--- 13/js/filters-functions.js
+++ 13/js/filters-functions.js
@@ -1,21 +1,19 @@
 import { createPictures } from './create-miniatures.js';
+import { shuffle, debounce, RERENDER_DELAY } from './util.js';
 const RANDOM_PICTURES_COUNT = 10;
+const filtersContainer = document.querySelector('.img-filters');
 const randomFilterButton = document.querySelector('#filter-random');
 const defaultFilterButton = document.querySelector('#filter-default');
 const discussedFilterButton = document.querySelector('#filter-discussed');
+const filterRandom = 'filter-random';
+const filterDefault = 'filter-default';
+const filterDiscussed = 'filter-discussed';
+const debounceCreatePictures = debounce(createPictures, RERENDER_DELAY);
 
 //функция, показывающая фильтры фото
 export const openFilters = () => {
   const fitters = document.querySelector('.img-filters');
   fitters.classList.remove('img-filters--inactive');
-};
-
-//Функция, которая перемешивает элементы массива случайным образом
-const shuffle = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
-    [array[i], array[j]] = [array[j], array[i]];
-  }
 };
 
 const likesCount = (object) => object.comments.length;
@@ -27,47 +25,49 @@ const compare = (picture1, picture2) => {
   return likes2 - likes1;
 };
 
-//функция, которая отрисовыввает рандомные фото пользователей
-export const createRandomPictures = (picturesFromServer) => {
+//функция, удаляющая активное состояние кнопки, после переключения на новый фильтр
+const removeActiveFilterStatus = () => {
   document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+};
+
+//функция, которая отрисовыввает рандомные фото пользователей
+const createRandomPictures = (picturesFromServer) => {
+  removeActiveFilterStatus();
   picturesFromServer = picturesFromServer.slice();
   shuffle(picturesFromServer);
   const newPicturesArray = picturesFromServer.slice(0, RANDOM_PICTURES_COUNT);
-  createPictures(newPicturesArray);
   randomFilterButton.classList.add('img-filters__button--active');
+  debounceCreatePictures(newPicturesArray);
 };
 
 //функция, которая отрисовыввает фото пользователей в порядке, полученном от сервера
-export const createDefaultPictures = (picturesFromServer) => {
-  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
-  createPictures(picturesFromServer);
+const createDefaultPictures = (picturesFromServer) => {
+  removeActiveFilterStatus();
   defaultFilterButton.classList.add('img-filters__button--active');
+  debounceCreatePictures(picturesFromServer);
 };
 
 //функция, которая отрисовыввает фото пользователей, сортируя их по убыванию количества комментариев
-export const createDiscussedPictures = (picturesFromServer) => {
-  document.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+const createDiscussedPictures = (picturesFromServer) => {
+  removeActiveFilterStatus();
   picturesFromServer = picturesFromServer.slice();
   picturesFromServer.sort(compare);
-  createPictures(picturesFromServer);
   discussedFilterButton.classList.add('img-filters__button--active');
+  debounceCreatePictures(picturesFromServer);
 };
 
-export const setRandomPictures = (cb) => {
-  randomFilterButton.addEventListener('click', () => {
-    cb();
+
+export const selectGenerateFunction = (data) => {
+  filtersContainer.addEventListener('click', (evt) => {
+    const currentFilter = evt.target.id;
+    if (currentFilter === filterRandom) {
+      return createRandomPictures(data);
+    }
+    if (currentFilter === filterDefault) {
+      return createDefaultPictures(data);
+    }
+    if (currentFilter === filterDiscussed) {
+      return createDiscussedPictures(data);
+    }
   });
 };
-
-export const setDefaultPictures = (cb) => {
-  defaultFilterButton.addEventListener('click', () => {
-    cb();
-  });
-};
-
-export const setDiscussedPictures = (cb) => {
-  discussedFilterButton.addEventListener('click', () => {
-    cb();
-  });
-};
-
--- 13/js/util.js
+++ 13/js/util.js
@@ -101,6 +101,14 @@ const debounce = (callback, timeoutDelay) => {
   };
 };
 
-export { isEscapeKey, showAlert, loadingErrorMessage, showSuccessMessage, debounce, RERENDER_DELAY };
+//Функция, которая перемешивает элементы массива случайным образом
+const shuffle = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1)); // случайный индекс от 0 до i
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+};
+
+export { isEscapeKey, showAlert, loadingErrorMessage, showSuccessMessage, debounce, RERENDER_DELAY, shuffle };
 
 
